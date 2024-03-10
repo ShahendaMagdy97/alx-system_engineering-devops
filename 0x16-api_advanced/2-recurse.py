572147c850a7fe5add2b45d3365a055af572cbd5
shahendamagdy97 <shahendamagdy97@yahoo.com>
2024-03-10 09:36:07 +0200

    modified

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API and returns a list
-containing the titles of all hot articles for a given subreddit."""
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
 
-def recurse(subreddit, hot_list=[], a=""):
-    """recursive function that queries the Reddit API and returns a
-    list containing the titles"""
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
 
-    headers = {'User-Agent': 'MyPythonScript/1.0'}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={a}'
-    request = requests.get(url=url, headers=headers, allow_redirects=False)
-    if request.status_code != 200:
-        return None
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        hot_list.extend(list_titles(
-            request.json().get('data').get('children')))
-        a = request.json().get('data').get('after')
-        if a is None:
-            return hot_list
-        return recurse(subreddit, hot_list, a)
-
-
-def list_titles(hot_list, titles=[]):
-    """list titles of hot posts"""
-    if len(hot_list) == 0:
-        return titles
-    else:
-        titles.append(hot_list[0].get('data').get('title'))
-        return list_titles(hot_list[1:], titles)
+        return (None)
